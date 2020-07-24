60434e7b1b9297beeac815d82c1617e793720293
rdwr <o_rdwr@qq.com>
2020-07-24 18:01:24 +0800

    ""

--- dwm/scripts/brightness_control.sh
+++ dwm/scripts/brightness_control.sh
@@ -1,10 +1,36 @@
 #!/bin/sh
 
+CURBR="$(xrandr --current --verbose|grep 'Brightness:'|head -1|awk '{print $2}')"
+STEP="0.05"
+MAXBR="1.0"
+MINBR="0"
+echo "current brightness" $CURBR
+
 case "$1" in
     U|u|[U,u]p)
-	xbacklight -inc 5
+        # increase brightness by 5%
+        _brightness=$(echo $CURBR+$STEP|bc)
+        echo "desired new brightness" $_brightness
+        _res=$(echo "$_brightness <= $MAXBR"|bc)
+        if [ "$_res" -eq 1 ]; then
+            echo "new brightness" $_brightness
+            xrandr --output eDP-1 --brightness $_brightness
+        fi
         ;;
     D|d|[D,d]own|[D,d]o)
-	xbacklight -dec 5
+        # increase brightness by 5%
+        _brightness=$(echo $CURBR-$STEP|bc)
+        echo "desired new brightness" $_brightness
+        _res=$(echo "$_brightness >= $MINBR"|bc)
+        if [ "$_res" -eq 1 ]; then
+            echo "new brightness" $_brightness
+            xrandr --output eDP-1 --brightness $_brightness
+        fi
+        ;;
+    *)
+        echo "Usage: $0 [up|down]"
+        exit 1
         ;;
 esac
+
+exit 0
